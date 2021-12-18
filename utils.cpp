dee5e552b4dd315e97b7ee05d168f7ebd290b566
Zhumakhan <zhumakhan.nazir@nu.edu.kz>
2021-12-19 00:20:42 +0600

    row major and column major matrices

--- utils.cpp
+++ utils.cpp
@@ -128,7 +128,7 @@ bool check_mul(T* a, T* b, T* c, int M, int K, int N, int order_type) {
             for (int k = 0; k < K; ++k) {
                 value += a[ IDX(i, k, M, K, order_type) ] * b[ IDX(k, j, K, N, order_type) ];
             }
-            if ( fabs(value - c[ IDX(i, j, M, N, order_type) ] ) > 0.01) {
+            if ( fabs(value - c[ IDX(i, j, M, N, order_type) ] ) > 0.1) {
                 std::cout << c[ IDX(i, j, M, N, order_type) ] << " " << value << std::endl;
                 return false;
             }
