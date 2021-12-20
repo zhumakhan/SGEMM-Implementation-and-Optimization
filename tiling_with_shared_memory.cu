c373a37aad35554bfb71dd5c2cc1ebcab2ba8668
Zhumakhan <zhumakhan.nazir@nu.edu.kz>
2021-12-20 13:56:20 +0600

    global memory only  optimized for caching

--- tiling_with_shared_memory.cu
+++ tiling_with_shared_memory.cu
@@ -3,7 +3,6 @@
 
 
 #define BS 16
-#define TILE_SIZE 16
 
 __global__ void mmShared(float *A, float *B, float *C, int M, int K, int N);
 
@@ -98,7 +97,7 @@ __global__ void mmShared(float *A, float *B, float *C, int M, int K, int N){
         // sB[ii][jj] = B[ IDXR(k+ii,j, K, N) ];
         
     //     __syncthreads();
-         for(m = 0; m < BS and k+m < K; ++m){
+         for(m = 0; m < BS and k+m < K; m++){
             // temp += sA[ii][m] * sB[m][jj];
             C[ IDXR(i,j,M,N) ] += A[ IDXR(i,k+m, M, K) ] *  B[ IDXR(k+m,j, K, N) ];
             if(k+m == prev+1)prev = k+m;
