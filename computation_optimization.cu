4db67566a7ef5c7a172ab7a6ea84914b464ce983
Zhumakhan <zhumakhan.nazir@nu.edu.kz>
2021-12-26 03:04:38 +0600

    column column

--- computation_optimization.cu
+++ computation_optimization.cu
@@ -5,3 +5,91 @@
 #include "utils.cpp"
 #include <stdio.h>
 
+#define TILE_SIZE 16
+#define VECTOR_SIZE 4
+
+/*
+    A is a row major matrix ( M x K )
+    B is a row major matrix ( K x N )
+
+    Asub: TILE_SIZE x TILE_SIZE
+    Bsub: TILE_SIZE x ( TILE_SIZE * VECTOR_SIZE )
+
+    dim3 threads ( TILE_SIZE, VECTOR_SIZE )
+    dim3 blocks  ( N / ( TILE_SIZE * VECTOR_SIZE ), M / TILE_SIZE )
+*/
+
+__global__ void mmCompOpt(float *A, float *B, float *C, int M, int K, int N){
+    
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    volatile __shared__ float As[ TILE_SIZE * TILE_SIZE ];
+
+    float Cv[ TILE_SIZE ] = { 0 };
+
+    int aBegin  = K * TILE_SIZE * by;
+    int aEnd    = aBegin + K;
+    int aStep   = TILE_SIZE;
+
+    int bBegin  = TILE_SIZE * VECTOR_SIZE * bx;
+    int bStep   = TILE_SIZE * N;
+
+    int i, j;
+    
+    float *aPtr, *bPtr;
+    float bValue;
+
+    // to avoid repeated computations 
+    // int t1 = tx * TILE_SIZE + ty;
+    // int t2 = ty * K + tx;
+    // int t3 = ty * TILE_SIZE + tx;
+    // int t4;
+
+    for(int a = aBegin, b = bBegin; a < aEnd; a += aStep, b += bStep){
+
+        for(i = 0; i < TILE_SIZE / VECTOR_SIZE; ++i){
+            // load elements to As in column major way from matrix A
+            // t4 = i * VECTOR_SIZE;
+            As[ tx * TILE_SIZE + ty + i * VECTOR_SIZE ] = A[ a + K * (i * VECTOR_SIZE + ty) + tx ];
+            // As[ t1 + t4 ] = A[ a + t4 * K + t2 ];
+        }
+        
+        __syncthreads();
+
+        aPtr = As;
+        bPtr = &B[ b + TILE_SIZE * ty + tx ];
+        // bPtr = &B[b + t3 ];
+
+        for(i = 0; i < TILE_SIZE; ++i){
+            bValue = *bPtr;
+
+            for(j = 0; j < TILE_SIZE; ++j){
+                Cv[ j ] += aPtr[ j ] * bValue;
+            }
+
+            aPtr += TILE_SIZE;
+            bPtr += N;
+        }
+
+        __syncthreads();
+
+    }
+
+    int c = N * TILE_SIZE * by + TILE_SIZE * VECTOR_SIZE * bx;
+    c += TILE_SIZE * ty + tx;
+    // c += t3;
+
+    for(i = 0; i < TILE_SIZE; ++i){
+        C[ c ] = Cv[ i ];
+        c += N;
+    }
+}
+
+
+
+
+
--- tiling_with_shared_memory.cu
+++ tiling_with_shared_memory.cu
@@ -18,12 +18,15 @@ void test(void (*kernel)(float *, float *, float *, int, int, int),
 
 
 int main(int argc, char *argv[]){
-    int M = std::atoi(argv[1]), K = std::atoi(argv[2]), N = std::atoi(argv[3]);
+    int M = std::atoi(argv[1]);
+    int K = std::atoi(argv[2]);
+    int N = std::atoi(argv[3]);
+
     printf("M=%d K=%d N=%d\n",M,K,N);
     
 
-    dim3 threads(TILE_SIZE,TILE_SIZE);
-    dim3 blocks( (N+threads.x-1)/threads.x, (M+threads.y-1)/threads.y);
+    dim3 threads(   TILE_SIZE, TILE_SIZE    );
+    dim3 blocks(    ( N + threads.x - 1 ) / threads.x, ( M + threads.y - 1 ) / threads.y   );
 
     std::cout << "Row Row" << std::endl;
     test(&mmSharedRR, threads, blocks, M, K, N, utils::ROW_MAJOR, utils::ROW_MAJOR, utils::ROW_MAJOR);
@@ -41,32 +44,32 @@ __global__ void mmSharedRR(float *A, float *B, float *C, int M, int K, int N){
     int bx = blockIdx.x, by = blockIdx.y;
     int tx = threadIdx.x, ty = threadIdx.y;
 
-    volatile __shared__ float As[TILE_SIZE][TILE_SIZE];
-    volatile __shared__ float Bs[TILE_SIZE][TILE_SIZE];
+    volatile __shared__ float As[ TILE_SIZE ][ TILE_SIZE ];
+    volatile __shared__ float Bs[ TILE_SIZE ][ TILE_SIZE ];
 
-    int aBegin = K * TILE_SIZE * by;
-    int aEnd = aBegin + K;
-    int aStep = TILE_SIZE;
+    int aBegin  = K * TILE_SIZE * by;
+    int aEnd    = aBegin + K;
+    int aStep   = TILE_SIZE;
 
-    int bBegin = TILE_SIZE * bx;
-    int bStep = TILE_SIZE * N;
+    int bBegin  = TILE_SIZE * bx;
+    int bStep   = TILE_SIZE * N;
 
     float Csub = 0;
 
     for (int i = aBegin, j = bBegin; i < aEnd; i += aStep, j += bStep) {
-        As[ty][tx] = A[i + K * ty + tx];
-        Bs[tx][ty] = B[j + N * tx + ty];
+        As[ ty ][ tx ] = A[ i + K * ty + tx ];
+        Bs[ tx ][ ty ] = B[ j + N * tx + ty ];
 
         __syncthreads();
 
         for (int k = 0; k < TILE_SIZE; ++k) {
-            Csub += As[ty][k]*Bs[k][tx];
+            Csub += As[ ty ][ k ] * Bs[ k ][ tx ];
         }
         
         __syncthreads();
     }
     int cIdx = N * TILE_SIZE * by + TILE_SIZE * bx;
-    C[cIdx + N * ty + tx] = Csub;
+    C[ cIdx + N * ty + tx ] = Csub;
 }
 
 __global__ void mmSharedRC(float *A, float *B, float *C, int M, int K, int N){
@@ -76,14 +79,14 @@ __global__ void mmSharedRC(float *A, float *B, float *C, int M, int K, int N){
     volatile __shared__ float As[TILE_SIZE][TILE_SIZE];
     volatile __shared__ float Bs[TILE_SIZE][TILE_SIZE];
 
-    int aBegin = K * TILE_SIZE * by;
-    int aEnd = aBegin + K;
-    int aStep = TILE_SIZE;
+    int aBegin  = K * TILE_SIZE * by;
+    int aEnd    = aBegin + K;
+    int aStep   = TILE_SIZE;
 
-    int bBegin = K * TILE_SIZE * bx;
-    int bStep = TILE_SIZE;
+    int bBegin  = K * TILE_SIZE * bx;
+    int bStep   = TILE_SIZE;
 
-    float Csub = 0;
+    float Csub  = 0;
 
     for (int i = aBegin, j = bBegin; i < aEnd; i += aStep, j += bStep) {
         As[ty][tx] = A[i + K * ty + tx];
@@ -108,12 +111,12 @@ __global__ void mmSharedCR(float *A, float *B, float *C, int M, int K, int N){
     volatile __shared__ float As[TILE_SIZE][TILE_SIZE];
     volatile __shared__ float Bs[TILE_SIZE][TILE_SIZE];
 
-    int aBegin = TILE_SIZE * by;
-    int aEnd = aBegin + K*M;
-    int aStep = TILE_SIZE * M;
+    int aBegin  = TILE_SIZE * by;
+    int aEnd    = aBegin + K*M;
+    int aStep   = TILE_SIZE * M;
 
-    int bBegin = TILE_SIZE * bx;
-    int bStep = TILE_SIZE * N;
+    int bBegin  = TILE_SIZE * bx;
+    int bStep   = TILE_SIZE * N;
 
     float Csub = 0;
 
@@ -139,14 +142,14 @@ __global__ void mmSharedCC(float *A, float *B, float *C, int M, int K, int N){
     volatile __shared__ float As[TILE_SIZE][TILE_SIZE];
     volatile __shared__ float Bs[TILE_SIZE][TILE_SIZE];
 
-    int aBegin = TILE_SIZE * by;
-    int aEnd = aBegin + K*M;
-    int aStep = TILE_SIZE * M;
+    int aBegin  = TILE_SIZE * by;
+    int aEnd    = aBegin + K*M;
+    int aStep   = TILE_SIZE * M;
 
-    int bBegin = K * TILE_SIZE * bx;
-    int bStep = TILE_SIZE;
+    int bBegin  = K * TILE_SIZE * bx;
+    int bStep   = TILE_SIZE;
 
-    float Csub = 0;
+    float Csub  = 0;
 
     for (int i = aBegin, j = bBegin; i < aEnd; i += aStep, j += bStep) {
         As[ty][tx] = A[i + M * tx + ty];
