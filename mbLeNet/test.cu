774febde7e0bf0062cfcd2c95799a20e691fa3fc
Zhumakhan <zhumakhan.nazir@nu.edu.kz>
2021-12-27 03:26:57 +0600

    compt optimization with column-major matrix A

--- mbLeNet/test.cu
+++ mbLeNet/test.cu
@@ -26,20 +26,6 @@
 
 */
 
-// typedef void (*FunctionPointer_t)(float *, float *, float *, const int, const int, const int);
-
-// class Kernel_t{
-// public:
-//     std::string name;
-//     FunctionPointer_t function;
-//     dim3 threads;
-//     dim3 blocks;
-
-//     Kernel_t(std::string name, FunctionPointer_t function, dim3 threads, dim3 blocks):name(name),function(function), threads(threads), blocks(blocks){
-//     }
-// };
-
-
 int main(int argc, char *argv[]){
     const int M = std::atoi(argv[1]);
     const int K = std::atoi(argv[2]);
@@ -55,10 +41,8 @@ int main(int argc, char *argv[]){
     float *B = utils::random_matrix_gpu<float>(K, N, utils::ROW_MAJOR,-50,50);
     float *C = (float*)malloc(sizeof(float)*M*N);
     
-    float ms;
     float *dA, *dB, *dC;
 
-    cudaEvent_t start, stop;
 
     cudaMalloc((void**)&dA,sizeof(float)*M*K);
     cudaMalloc((void**)&dB,sizeof(float)*K*N);
@@ -73,66 +57,22 @@ int main(int argc, char *argv[]){
     const dim3 threads2( TILE_SIZE, VECTOR_SIZE );
     const dim3 blocks2(N / (TILE_SIZE * VECTOR_SIZE), M / TILE_SIZE);
     
-    // const Kernel_t kernels [ 4 ] = {
-    //     {   "tiling", &mmSharedRR, threads1, blocks1      },
-    //     {   "comp_opt", &mmCompOpt_v1, threads2, blocks2    },
-    //     {   "unrolling", &mmLoopUnrolling, threads2, blocks2 },
-    //     {   "prefetching", &mmPrefetching, threads2, blocks2   }
-    // };
 
-    // for(int i = 0; i < 4; i++){
+    mmSharedRR <<< blocks1, threads1 >>> ( dA, dB, dC, M, K, N );
 
-        // sleep(5);
+    mmCompOpt_v1 <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
 
-        // cudaEventCreate(&start);
-        // cudaEventCreate(&stop);
-        // cudaEventRecord(start);
+    mmLoopUnrolling <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
 
-        mmSharedRR <<< blocks1, threads1 >>> ( dA, dB, dC, M, K, N );
-
-        mmCompOpt_v1 <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
-
-        mmLoopUnrolling <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
-
-        mmPrefetching <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
+    mmPrefetching <<< blocks2, threads2 >>> ( dA, dB, dC, M, K, N );
         
-        // cudaEventRecord(stop);
-        // cudaEventSynchronize(stop);
-        // cudaEventElapsedTime(&ms, start, stop);
-        // cudaEventDestroy(start);
-        // cudaEventDestroy(stop);
         
-        cudaError_t cuda_error = cudaGetLastError();
-        if(cuda_error != cudaSuccess)
-        {
-          printf("CUDA error: %s\n", cudaGetErrorString(cuda_error));
-          exit(-1);
-        }
-
-        // bool correct = true;
-        
-// #ifdef CHECK
-//         cudaMemcpy(C,dC,sizeof(float)*N*M, cudaMemcpyDeviceToHost);
-//         correct = utils::check_mul<float>(A, B, C, M, K, N, utils::ROW_MAJOR, utils::ROW_MAJOR, utils::ROW_MAJOR);
-// #endif
-
-//         std::cout << kernels[i].name << " " << ms << " " << correct << std::endl;
-//     }
-
-
-// #ifdef DEBUG
-
-// #ifndef CHECK
-//     cudaMemcpy(C,dC,sizeof(float)*N*M, cudaMemcpyDeviceToHost);
-// #endif
-
-//     std::cout << "Matrix A:" << std::endl;
-//     utils::print_mat_gpu(a, M, K, utils::ROW_MAJOR);
-//     std::cout << "Matrix B:" << std::endl;
-//     utils::print_mat_gpu(b, K, N, utils::ROW_MAJOR);
-//     std::cout << "Matrix C:" << std::endl;
-//     utils::print_mat_gpu(c, M, N, utils::ROW_MAJOR);
-// #endif
+    cudaError_t cuda_error = cudaGetLastError();
+    if(cuda_error != cudaSuccess)
+    {
+      printf("CUDA error: %s\n", cudaGetErrorString(cuda_error));
+      exit(-1);
+    }
 
     cudaFree(dA);
     cudaFree(dB);
